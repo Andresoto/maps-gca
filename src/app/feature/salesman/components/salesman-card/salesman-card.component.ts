3b87d3d327ef686b459f009807c19e26766be441
Andresoto <zuluandres27@gmail.com>
2024-02-15 18:17:32 -0500

    Variables scss

--- src/app/feature/salesman/components/salesman-card/salesman-card.component.ts
+++ src/app/feature/salesman/components/salesman-card/salesman-card.component.ts
@@ -9,19 +9,4 @@ import { Salesman } from '@core/models/salesman.model';
 export class SalesmanCardComponent {
 
   @Input() salesmanData!: Salesman;
-
-  // public salesmanData: Salesman = {
-  //   "id": "002",
-  //   "name": "Miguel",
-  //   "category": "Operador",
-  //   "address": "Cra 62 32-24",
-  //   "isActive": false,
-  //   "coordinates": {
-  //     "latitude": 4.6811127,
-  //     "longitude": 4.6072127,
-  //     "height": 3.0
-  //   },
-  //   "photo": "person1",
-  //   "vehicle": "moto"
-  // }
 }
