25d75bc28482e57b9572bb4d8362fbe9473981c3
Andresoto <zuluandres27@gmail.com>
2024-02-16 12:41:55 -0500

    Metodo para crear un nuevo vendedor

--- src/app/shared/services/salesman.service.ts
+++ src/app/shared/services/salesman.service.ts
@@ -1,19 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Salesman } from '@core/models/salesman.model';
+import { NewSalesman, Salesman } from '@core/models/salesman.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SalesmanService {
 
-  private baseURL = 'http://74.235.109.154/api/'
+  private baseURL = 'http://74.235.109.154/api'
 
   constructor(
     private http: HttpClient
   ) { }
 
   getAllSalesman() {
-    return this.http.get<Salesman[]>(`${this.baseURL}salesman`);
+    return this.http.get<Salesman[]>(`${this.baseURL}/salesman`);
+  }
+
+  createSalesman(data: NewSalesman) {
+    return this.http.post(`${this.baseURL}/salesman`, data);
   }
 }
