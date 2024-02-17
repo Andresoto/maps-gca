1d2fac1d52f6573dac0b6e89163fb2dc5c52f1f7
Andresoto <zuluandres27@gmail.com>
2024-02-16 22:04:59 -0500

    Se agrego imagen e icon default

--- src/app/feature/salesman/components/salesman-card/salesman-card.component.ts
+++ src/app/feature/salesman/components/salesman-card/salesman-card.component.ts
@@ -11,7 +11,37 @@ export class SalesmanCardComponent {
   @Input() salesmanData!: Salesman;
   @Input() active: boolean = false;
 
+  allImages: string[] = [
+    'person1',
+    'person2',
+    'person3',
+    'person4',
+    'person5',
+    'person6',
+  ]
+
+  allIcons: string[] = [
+    'ambulancia',
+    'carro',
+    'grua',
+    'moto',
+    'pin1',
+    'pin2',
+    'pin3',
+    'pin4',
+    'pin10',
+    'sinvehiculo',
+  ]
+
   focusCard() {
     this.active = !this.active;
+  }
+
+  validateImage(image: string) {
+    return this.allImages.includes(image);
+  }
+
+  validateIcon(icon: string) {
+    return this.allIcons.includes(icon);
   }
 }
